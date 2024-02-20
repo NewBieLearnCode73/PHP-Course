86de87b8df8e32b6d9b4073557a344dbe73d664e
DinhChieu_73 <ndchieu73@gmail.com>
2024-02-20 16:01:44 +0700

    Thêm thư viện Jquery

---
+++ Chapter_23/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="./js/app.js"></script>
+</head>
+
+<body>
+    <a href="" id="click_me">Click me</a>
+</body>
+
+</html>
---
+++ Chapter_23/note/ajax.md
@@ -0,0 +1,6 @@
+# Ứng dụng Ajax
+
+-   Giúp cập nhật thông tin của 1 widget hoặc 1 block nào đó mà không cần reload lại trang web
+-   Cách hoạt động của Ajax:
+
+![alt text](image.png)
